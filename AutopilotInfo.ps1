b3911690645a2dec01385fa66ec5d307902e83ee
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-13 10:33:43 +0200

    Add Comment

--- AutopilotInfo.ps1
+++ AutopilotInfo.ps1
@@ -6,4 +6,4 @@
 #Installation des Scripts 
 # 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force #Package-Provider installieren damit das Script für die Hardware-Hashes ausgeführt werden kann
-Install-Script Get-WindowsAutoPilotInfo                             
+Install-Script Get-WindowsAutoPilotInfo           # Instlallieren des Scirpts zum auslesen der Hardware-Hashes                 
