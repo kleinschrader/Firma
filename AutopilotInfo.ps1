2460c6c68370949983c20ea76339cd27ae8c436e
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-13 14:23:59 +0200

    Löppt jetzt.

--- AutopilotInfo.ps1
+++ AutopilotInfo.ps1
@@ -4,7 +4,6 @@
 # für Windows Powershell
 #-----------#Installation des Scripts ----------------------------------------------------
 
-Powershell                                                                  # Starten der Powershell 
 Install-PackageProvider -Name NuGet -Confirm -ErrorAction SilentlyContinue  # Package-Provider installieren damit das Script für die Hardware-Hashes ausgeführt werden kann
 Install-Script Get-WindowsAutoPilotInfo -ErrorAction SilentlyContinue       # Instlallieren des Scirpts zum auslesen der Hardware-Hashes
 Import-Module Microsoft.PowerShell.Management
@@ -15,7 +14,7 @@ Import-Module Microsoft.PowerShell.Management
 $Hash = get-WindowsAutoPilotInfo -ErrorAction SilentlyContinue #Auslesen des Hardware-Hashes
 
 #------------------#Abfragen der benötigten Informationen zum einfügen der Daten in die .CSV ------------------------------------------------------------------------
-$SN  = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+$SN  = (Get-CimInstance -Class Win32_BIOS).SerialNumber
 $ProductKey = wmic path softwarelicensingservice get OA3xOriginalProductKey
 # $UPN = Read-Host("Hier die eigene UPN angeben. Wenn unbekannt, UNBEDINGT unterlassen und erst erfragen.") || "Aufgepasst mit dem Dingens, MS mag da keine falschen Inputs".upper
 
@@ -26,9 +25,9 @@ $ProductKey = wmic path softwarelicensingservice get OA3xOriginalProductKey
 
 $usbdriveGUID = "0002941a-0000-0000-0000-100000000000"
 
-$fullPath = (Get-Volume -Path "\\?\Volume{$usbdriveGUID}\").driveLetter+":\InTune"   
+$fullPath = (Get-Volume -Path "\\?\Volume{$usbdriveGUID}\").driveLetter+":\InTune\"   
 $exportObj = New-Object psobject -Property @{"Device Serial number" = $SN;"Windows Product ID" = $ProductKey; "Hardware Hash" =$Hash;"Group Tag" = ""; "Assigned User" = ""}
-$exportObj | export-csv -path $fullPath + "NewDevice$SN.csv" 
+$exportObj | export-csv -path ($fullPath + "NewDevice$SN.csv") 
 
 
 
