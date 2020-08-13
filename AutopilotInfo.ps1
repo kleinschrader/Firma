682f3476d2f251a73f2258f596fcd418451b95a7
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-13 10:31:00 +0200

    First Init

---
+++ AutopilotInfo.ps1
@@ -0,0 +1,9 @@
+# Script zum Installieren, auslesen und einfügen von Geräten unter Windows in Intune
+# von Nicolas Csaba Bohocki
+# 13.08.2020
+# für Windows Powershell
+#------------------------------------------------------------------------------------------
+#Installation des Scripts 
+# 
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force #Package-Provider installieren damit das Script für die Hardware-Hashes ausgeführt werden kann
+Install-Script Get-WindowsAutoPilotInfo                             
