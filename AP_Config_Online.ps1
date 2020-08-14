a9a2bed790ff12265d44f711f97bf9a881f7f77c
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-14 14:45:15 +0200

    Form geben. Maschinendialog hinzugefügt.

--- AP_Config_Online.ps1
+++ AP_Config_Online.ps1
@@ -1,7 +1,19 @@
+#------------Installieren der benötigten Packages----------------------
+"Installieren der bentötigten Pakete"
+"Paket 1 von 4"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+"Fertig"
+"Paket 2 von 4"
 Install-Module AzureAD -Force
+"Fertig"
+"Paket 3 von 4"
 Install-Module WindowsAutopilotIntune -Force
+"Fertig"
+"Paket 4 von  4"
 Install-Module Microsoft.Graph.Intune -Force
-$UPN = Read-Host -Prompt "Please insert your UPN"
+"Alle Pakete installiert" 
+#------------Verbinden mit InTune--------------------------------------
+"Es wird eine Verbindung zu InTune aufgebaut."
+$UPN = Read-Host -Prompt "Bitte geben sie ihre UPN ein."
 $AComputerName = HOSTNAME.EXE
 Get-WindowsAutoPilotInfo.ps1 -AssignedUser "$UPN" -Append -Online -AddToGroup "SG MDM Test Geräte" -AssignedComputerName "$AComputername" -Assign
