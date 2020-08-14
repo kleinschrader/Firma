866ccd9a1ad6460bf6696450176f3fd37959906d
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-14 08:34:41 +0200

    -force Argumente entfernt, -ErrorActions eingefügt

--- Autopilot_Beitritt_Nachtraeglich.ps1
+++ Autopilot_Beitritt_Nachtraeglich.ps1
@@ -1,8 +1,8 @@
 #------------Installieren der benötigten Packages----------------------
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module AzureAD -Force
-Install-Module WindowsAutopilotIntune -Force
-Install-Module Microsoft.Graph.Intune -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -ErrorAction Continue -
+Install-Module AzureAD -ErrorAction Continue
+Install-Module WindowsAutopilotIntune -ErrorAction Continue
+Install-Module Microsoft.Graph.Intune -ErrorAction Continue
 #------------Verbinden mit InTune--------------------------------------
 Connect-MSGraph
 #------------Erstellen einer JSON-FILE für nachträglichen Zugang-------
