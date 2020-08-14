3186136dec16c023e67b72bf829844b8cde40228
Nico Heinemann [TechnoSoft Consulting GmbH] <nheinemann@tcg-online.de>
2020-08-14 11:31:49 +0200

    Edit added Pakages

--- AP_Config_Online.ps1
+++ AP_Config_Online.ps1
@@ -1,4 +1,7 @@
-
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module AzureAD -Force
+Install-Module WindowsAutopilotIntune -Force
+Install-Module Microsoft.Graph.Intune -Force
 $UPN = Read-Host -Prompt "Please insert your UPN"
 $AComputerName = HOSTNAME.EXE
 Get-WindowsAutoPilotInfo.ps1 -AssignedUser "$UPN" -Append -Online -AddToGroup "SG MDM Test Geräte" -AssignedComputerName "$AComputername" -Assign
