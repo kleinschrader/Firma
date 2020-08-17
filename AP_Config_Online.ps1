bfb7370c22b20b42c0b432d8b84b95e724a83e90
Nicolas Bohocki [TechnoSoft Consulting GmbH] <nbohocki@tcg-online.de>
2020-08-17 11:16:53 +0200

    Fehlendes Paket nachgereicht.

--- AP_Config_Online.ps1
+++ AP_Config_Online.ps1
@@ -1,16 +1,19 @@
 #------------Installieren der benötigten Packages----------------------
 "Installieren der bentötigten Pakete"
-"Paket 1 von 4"
+"Paket 1 von 5"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 "Fertig"
-"Paket 2 von 4"
+"Paket 2 von 5"
 Install-Module AzureAD -Force
 "Fertig"
-"Paket 3 von 4"
+"Paket 3 von 5"
 Install-Module WindowsAutopilotIntune -Force
 "Fertig"
-"Paket 4 von  4"
+"Paket 4 von 5"
 Install-Module Microsoft.Graph.Intune -Force
+"Fertig"
+"Paket 5 von 5"
+Install-Script Get-WindowsAutoPilotInfo -Force
 "Alle Pakete installiert" 
 #------------Verbinden mit InTune--------------------------------------
 "Es wird eine Verbindung zu InTune aufgebaut."
